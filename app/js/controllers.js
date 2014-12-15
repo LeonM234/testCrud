e9cddba287da22255f1e74a7a5c2a4e18fed2e70
Leon Peck <leonpeck234@gmail.com>
2014-12-15 15:52:21 -0600

    Controllers wasn't added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,18 @@
   "use strict";
 
   angular.module('ourApp')
-    .controller('ChristmasController', function(){
+    .controller('ChristmasController', function($http, $routeParams){
       var vm = this;
-      
+      var id = $routeParams.id;
+      var url = "https://testcrud.firebaseio.com/wishes.json";
+      var ref = new Firebase("https://testcrud.firebaseio.com/")
+
+    //  vm.getList = function(){
+        $http.get(url)
+          .success(function(data){
+            vm.wishes = data;
+          })
+      //}
 
     })
 
